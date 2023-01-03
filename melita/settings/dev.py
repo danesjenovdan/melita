19bff189f8e02ed543cadb9fdfffc2dbfbafffc6
LepkoQQ <lepko.san@gmail.com>
2023-01-03 12:17:05 +0100

    add project scaffold

---
+++ melita/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-5_qbev_#!)ccm7@rqzfqm$al(-_=y(uyoy8u7+v!8@((f2u)qh"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
