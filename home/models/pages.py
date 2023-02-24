ad61d649cb1295ae20b6abc95bfdf3715d90a3ad
LepkoQQ <lepko.san@gmail.com>
2023-02-24 15:57:46 +0100

    add generic page

--- home/models/pages.py
+++ home/models/pages.py
@@ -1,6 +1,22 @@
 from django.db import models
-from wagtail.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    pass
+    parent_page_types = []
+
+
+class GenericPage(Page):
+    body = StreamField(
+        [
+            ("paragraph", blocks.RichTextBlock(label="Rich text")),
+        ],
+        use_json_field=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
