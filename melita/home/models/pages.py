cdc521f57beeaa4f7fc90948175302cd0672f9f0
LepkoQQ <lepko.san@gmail.com>
2023-03-08 13:08:07 +0100

    update requirements

--- melita/home/models/pages.py
+++ melita/home/models/pages.py
@@ -1,8 +1,8 @@
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.core import blocks
-from wagtail.core.fields import StreamField
-from wagtail.core.models import Page
+from wagtail.admin.panels import FieldPanel
+from wagtail import blocks
+from wagtail.fields import StreamField
+from wagtail.models import Page
 
 
 class HomePage(Page):
