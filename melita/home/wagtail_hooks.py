0bd3f946580666edfbeeeddbdac5066823648386
LepkoQQ <lepko.san@gmail.com>
2023-03-09 18:00:19 +0100

    more lesson view design

--- melita/home/wagtail_hooks.py
+++ melita/home/wagtail_hooks.py
@@ -31,6 +31,9 @@ class LessonAdmin(ModelAdmin):
     search_fields = ("title", "program_type__name", "language__name")
 
 
+lesson_admin = LessonAdmin()
+
+
 class ProgramTypeAdmin(ModelAdmin):
     model = ProgramType
     menu_icon = "list-ul"
@@ -108,3 +111,35 @@ def global_admin_css():
     return format_html(
         '<link rel="stylesheet" href="{}">', static("css/custom_admin.css")
     )
+
+
+class UserbarEditLessonItem:
+    def render(self, request):
+        if "lesson" not in request.resolver_match.url_name:
+            return ""
+
+        try:
+            lesson_id = request.resolver_match.kwargs["id"]
+        except KeyError:
+            return ""
+
+        if not lesson_id:
+            return ""
+
+        edit_url = lesson_admin.url_helper.get_action_url("edit", lesson_id)
+        return format_html(
+            """
+            <li class="w-userbar__item " role="presentation">
+                <a href="{}" target="_parent" role="menuitem" tabindex="-1">
+                    <svg class="icon icon-edit w-action-icon" aria-hidden="true"><use href="#icon-edit"></use></svg>
+                    Edit this lesson
+                </a>
+            </li>
+            """,
+            edit_url,
+        )
+
+
+@hooks.register("construct_wagtail_userbar")
+def add_edit_lesson_item(request, items):
+    return items.insert(1, UserbarEditLessonItem())
