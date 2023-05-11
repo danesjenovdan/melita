8160841084e3a5aafb6c528c2b8361a40dc31a91
LepkoQQ <lepko.san@gmail.com>
2023-05-11 11:34:51 +0200

    dont use pvc, enable s3 for uploads

--- melita/melita/settings/k8s.py
+++ melita/melita/settings/k8s.py
@@ -23,6 +23,19 @@ DATABASES = {
 ALLOWED_HOSTS = ["localhost", "melita.lb.djnd.si"]
 CSRF_TRUSTED_ORIGINS = ["https://melita.lb.djnd.si"]
 
+# DJANGO STORAGE SETTINGS
+if os.getenv("DJANGO_ENABLE_S3", False):
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_ACCESS_KEY_ID = os.getenv("DJANGO_AWS_ACCESS_KEY_ID", "")
+    AWS_SECRET_ACCESS_KEY = os.getenv("DJANGO_AWS_SECRET_ACCESS_KEY", "")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("DJANGO_AWS_STORAGE_BUCKET_NAME", "djnd")
+    AWS_DEFAULT_ACL = "public-read"  # if files are not public they won't show up for end users
+    AWS_QUERYSTRING_AUTH = False  # query strings expire and don't play nice with the cache
+    AWS_LOCATION = os.getenv("DJANGO_AWS_LOCATION", "melita")
+    AWS_S3_REGION_NAME = os.getenv("DJANGO_AWS_REGION_NAME", "fr-par")
+    AWS_S3_ENDPOINT_URL = os.getenv("DJANGO_AWS_S3_ENDPOINT_URL", "https://s3.fr-par.scw.cloud")
+    AWS_S3_SIGNATURE_VERSION = os.getenv("DJANGO_AWS_S3_SIGNATURE_VERSION", "s3v4")
+    AWS_S3_FILE_OVERWRITE = False  # don't overwrite files if uploaded with same file name
 
 try:
     from .local import *
